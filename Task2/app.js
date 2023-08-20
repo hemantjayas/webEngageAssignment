7f46bf13e2056642dca2481e20bf68683a21574a
Hemant Kumar Jayas <hemantjayas03@gmail.com>
2023-08-20 13:04:26 +0530

    learned the if else and for loop in nonjucks and compled the task 2

--- Task2/app.js
+++ Task2/app.js
@@ -1,14 +1,37 @@
 const nunjucks = require('nunjucks');
+const http = require("http");
+const axios = require("axios")
+const PORT = 3000;
 
 // Configure Nunjucks to load templates from the current directory
 nunjucks.configure(__dirname, {
     autoescape: true,
 });
 
-// Render the template with data
-const renderedHtml = nunjucks.render('index.njk', {
-    greeting: 'Hello from Nunjucks!',
-    para: "hello hemant how are you this is written form the app.js file"
-});
+let users = [];
+const getData = async () => {
+    const resp = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    console.log(resp.data);
+    users = [...resp.data]
+}
 
-console.log(renderedHtml);
+console.log(users)
+
+
+// creating a server
+
+const httpServer = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader("content-type", "text/html");
+
+    const renderedHtml = nunjucks.render('users.njk', {
+        users: users,
+    });
+
+    res.end(renderedHtml)
+})
+
+httpServer.listen(PORT, async () => {
+    await getData()
+    console.log(`listening on port ${PORT}`)
+})
